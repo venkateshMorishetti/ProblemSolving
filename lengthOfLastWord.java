012d8a7ed5f4c59cf7e1df808a59de6ebb171fba
venkateshMorishetti <117025754+venkateshMorishetti@users.noreply.github.com>
2023-11-30 08:48:37 +0530

    problem on length of last word

--- lengthOfLastWord.java
+++ lengthOfLastWord.java
@@ -13,18 +13,15 @@ Example 1:
 Input: s = "Hello World"
 Output: 5
 Explanation: The last word is "World" with length 5.
-Example 2:
-
-Input: s = "   fly me   to   the moon  "
-Output: 4
-Explanation: The last word is "moon" with length 4.
 
 **/
 
-
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] a=s.split(" ");
-        return a[a.length-1].length(); 
+
+        s = s.trim();
+        String[] a = s.split(" ");
+        return a[a.length-1].trim().length();
+        
     }
 }
