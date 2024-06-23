1d753e90a4ea765f4f5db0c4fb6a982991200821
venkateshMorishetti <117025754+venkateshMorishetti@users.noreply.github.com>
2024-06-23 12:03:22 +0530

    268. Missing Number

--- missingNumber.java
+++ missingNumber.java
@@ -1,23 +1,36 @@
 /**
-Given an array of size N-1 such that it only contains distinct integers in the range of 1 to N. Find the missing element.
+
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+ 
 
 Example 1:
-Input:
-N = 5
-A[] = {1,2,3,5}
-Output: 4
+
+Input: nums = [3,0,1]
+Output: 2
+Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+Example 2:
+
+Input: nums = [0,1]
+Output: 2
+Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
 
 **/
 
-
 class Solution {
-    int missingNumber(int array[], int n) {
-        // Your Code Here
-        int sum = 0;
-        for(int i = 0; i < array.length; i++){
-            sum += array[i];
+    public int missingNumber(int[] nums) {
+        int s1 = 0, s2 = 0;
+
+        for(int i = 0; i <= nums.length; i++){
+            s1 = s1 ^ i;
         }
+        for(int i = 0; i < nums.length; i++){
+            s2 = s2 ^  nums[i];
+        }
+        System.out.println(s1);
+        System.out.println(s2);
+        return s1 ^ s2;
+
         
-        return (n*(n+1)/2)-sum;
     }
 }
